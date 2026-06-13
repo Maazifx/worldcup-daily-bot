56b93d1d0e2186d9bcbfe919347d7f526d74c207
Maazifx <123500181+Maazifx@users.noreply.github.com>
2026-06-13 18:20:25 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import feedparser
+import requests
+
+BOT_TOKEN = "8805510514:AAGIgIviJztRvh0iEXCRAQH-L8NtROciG2s"
+CHAT_ID = "wcupdates2026"
+
+feed = feedparser.parse(
+    "https://feeds.bbci.co.uk/sport/football/rss.xml"
+)
+
+latest = feed.entries[0]
+
+message = f"""
+🚨 {latest.title}
+
+{latest.link}
+"""
+
+requests.post(
+    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
+
+print("News sent")
